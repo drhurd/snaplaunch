fefb0f7a73e1fd02b303ead5c5003a7d5ab0db0c
Dylan Hurd <drhurd@umich.edu>
2015-05-04 11:46:31 -0400

    avoid deprecation warning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,13 +14,12 @@ Prelaunchr::Application.configure do
   config.eager_load = true
 
   # Serve up CSS and JS
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
